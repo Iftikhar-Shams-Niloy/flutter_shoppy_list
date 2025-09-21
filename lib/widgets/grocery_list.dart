df09277e28aec25b9def4b0faf2c79bad1bf8f75
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2025-09-21 17:16:09 +0600

    grocery list widget made

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppy_list/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Grocery List')),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return ListTile(
+            title: Text(groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: groceryItems[index].category.color,
+            ),
+            trailing: Text(groceryItems[index].quantity.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
