7cbe271e94de7f20f831f67540c008b4dad081a4
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2025-09-21 17:52:29 +0600

    new_list updated but consists error

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/categories.dart';
+
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
@@ -16,7 +18,58 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(title: const Text('Add New Item')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Text("THE FORM GOES HERE!"),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text('Name')),
+                //* <--- Validator to check if the input is valid -->
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      maxLength: 10,
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: "1",
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
